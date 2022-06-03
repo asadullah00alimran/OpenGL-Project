c7fd931ecbf9eaaa8a74df1b1f035fd651b33840
asadullah00imran <asadullahalimran00@gmail.com>
2022-06-03 23:34:05 +0600

    Basic Shape

---
+++ OpenGL Programming/basic shape.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+void display() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glOrtho(0, 10, 0, 10, 0, 10);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_LINE_STRIP);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(0, 3);
+    glVertex2f(0, 5);
+    glVertex2f(3, 5);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("Simple Line Strip");
+    glutInitWindowSize(320, 320);
+    glutInitWindowPosition(50, 50);
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
